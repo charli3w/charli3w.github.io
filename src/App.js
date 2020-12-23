85b92de96a6150fe381debde304e2a5a5f9ef8db
charli3w <charliehuawang@gmail.com>
2020-12-22 19:20:49 -0800

    skeleton for website

--- src/App.js
+++ src/App.js
@@ -3,22 +3,25 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id="app">
+      <Sidebar>
+        <div id="about"></div>
+        <div id="nav">/* TODO */</div>
+      </Sidebar>
+      <Main></Main>
     </div>
+  );
+}
+
+function Sidebar() {
+  return (
+    <div id="sidebar"></div>
+  );
+}
+
+function Main() {
+  return (
+    <div id="Main"></div>
   );
 }
 
