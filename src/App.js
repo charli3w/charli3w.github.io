926440d95b811912bad8700bab38ccd2496fd121
charli3w <charliehuawang@gmail.com>
2020-12-22 21:36:58 -0800

    Very basic left nav components

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
+import Nav from 'react-bootstrap/Nav'
+import profilePic from './assets/profile-pic.jpg'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons'
+import { faEnvelope, faFile } from '@fortawesome/free-solid-svg-icons'
 
 function App() {
   return (
     <div id="app">
-      <Sidebar>
-        <div id="about"></div>
-        <div id="nav">/* TODO */</div>
-      </Sidebar>
+      <Sidebar></Sidebar>
       <Main></Main>
     </div>
   );
@@ -15,7 +16,29 @@ function App() {
 
 function Sidebar() {
   return (
-    <div id="sidebar"></div>
+    <div id="sidebar" className="sidebar">
+      <div id="about">
+        <div className="portrait" style={{backgroundImage: `url(${profilePic})`}}></div>
+        <span>Hi, I'm Charlie. Welcome to my website!</span>
+        <div className="icons">
+          <FontAwesomeIcon icon={faLinkedin} />
+          <FontAwesomeIcon icon={faGithub} />
+          <FontAwesomeIcon icon={faEnvelope} />
+          <FontAwesomeIcon icon={faFile} />
+        </div>
+      </div>
+      <Nav id="nav" className="flex-column">
+        <Nav.Item>
+          <Nav.Link href="#">About Me</Nav.Link>
+        </Nav.Item>
+        <Nav.Item>
+          <Nav.Link href="#">Portfolio</Nav.Link>
+        </Nav.Item>
+        <Nav.Item>
+          <Nav.Link href="#">Contact</Nav.Link>
+        </Nav.Item>
+      </Nav>
+    </div>
   );
 }
 
